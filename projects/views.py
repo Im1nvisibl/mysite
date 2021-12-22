e5e512d6a697883e435f07c7cb238ee84363f65a
Invisible <vitaliysomoniuk@gmail.com>
2021-12-22 20:01:52 +0200

    Added templates and rendering data to templates

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+projectsList = [
+    {
+    'id':'1',
+    'title':"Apple1",
+    'description':'Is the best company'
+    },
+    {
+    'id':'2',
+    'title':"Apple2",
+    'description':'Is the best company'
+    },
+    {
+    'id':'3',
+    'title':"Apple3",
+    'description':'Is the best company'
+    },
+]
+
 def projects(request):
-    return HttpResponse('Here are our products')
+    page = 'Projects'
+    context = {'projects': projectsList}
+    return render(request, 'projects/projects.html',context)
 
 def project(request, pk):
-    return HttpResponse('Single project' + ' ' +str(pk))
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
